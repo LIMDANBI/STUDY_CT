b9ebe6e68e2936050ccc2bf96440a8db343caeab
LIMDANBI <dlaeksql08222@gmail.com>
2022-12-28 05:58:26 +0900

    스타트링크(그래프 이론/그래프 탐색/너비 우선 탐색)

--- BOJ/5014.cpp
+++ BOJ/5014.cpp
@@ -24,20 +24,20 @@ int main(){
             ans = cnt; // 눌러야 하는 버튼의 수의 최솟값
             break;
         }
-        else{
-            if(floor+U <= F){ // 범위를 벗어나지 않는 경우 
-                if(!visited[floor+U]){ // 이전에 방문하지 않은 경우
-                    visited[floor+U] = true;
-                    q.push({floor+U, cnt+1});
-                }
-            }
-            if(floor-D > 0){  // 범위를 벗어나지 않는 경우 
-                if(!visited[floor-D]){ // 이전에 방문하지 않은 경우
-                    visited[floor-D] = true;
-                    q.push({floor-D, cnt+1});
-                }
+        
+        if(floor+U <= F){ // 범위를 벗어나지 않는 경우 
+            if(!visited[floor+U]){ // 이전에 방문하지 않은 경우
+                visited[floor+U] = true;
+                q.push({floor+U, cnt+1});
             }
         }
+        if(floor-D > 0){  // 범위를 벗어나지 않는 경우 
+            if(!visited[floor-D]){ // 이전에 방문하지 않은 경우
+                visited[floor-D] = true;
+                q.push({floor-D, cnt+1});
+            }
+        }
+
     }
 
     if(ans == MAX) cout << "use the stairs";
