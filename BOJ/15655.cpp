e7f992542ec1e2fe6a1c05004fd0dab16b47285d
LIMDANBI <dlaeksql08222@gmail.com>
2023-01-14 14:52:31 +0900

    N과 M (6) (백트래킹)

---
+++ BOJ/15655.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm>
+#define MAX 10
+using namespace std;
+
+int N, M;
+bool isused[MAX];
+int num[MAX], ans[MAX];
+
+void input() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) cin >> num[i];
+	sort(num, num + N);
+}
+
+void solution(int idx, int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) cout << ans[i] << " ";
+		cout << "\n";
+		return;
+	}
+	for (int i = idx; i < N; i++) {
+		ans[cnt] = num[i];
+		solution(i + 1, cnt + 1);
+	}
+}
+
+int main() {
+	input();
+	solution(0, 0);
+}
