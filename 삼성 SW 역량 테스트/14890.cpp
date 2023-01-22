e29aafae905da767b7fdfce0c2f088f0e4ea98d7
LIMDANBI <dlaeksql08222@gmail.com>
2023-01-22 16:09:05 +0900

    경사로(구현) - code 최적화

--- 삼성 SW 역량 테스트/14890.cpp
+++ 삼성 SW 역량 테스트/14890.cpp
@@ -16,90 +16,62 @@ void solution(){
 
     int ans = 0;
     
-    // row
-    for(int c=0; c<N ; c++){
+    // row check
+    for(int r=0; r<N ; r++){
         
-        int l=0;
-        bool flag = true;
-        bool upToDown = false;
-        for(int r=0; r<N; r++){
-            if(r!=0 && map[r-1][c] != map[r][c]){ // 이전 칸과 높이가 같지 않은 경우
-                if(map[r-1][c]-map[r][c] == 1){ // \ (위->아래)
-                    if(upToDown){ // 아직 이전 경사로를 놓지 못한 경우
-                        flag = false;
-                        break;
-                    }
-                    if(L!=1){
-                        upToDown = true; // 위->아래 경사로 놓아야 함
-                        l = 1;
-                    }
-                    else l = 0;
-                }
-                else if(map[r-1][c]-map[r][c] == -1){ // / (아래->위)
-                    if(l>=L && !upToDown) l = 1; // 아래->위로 경사로를 놓을 수 있는 경우
-                    else{ // 경사로를 놓을 수 없는 경우
-                        flag = false;
-                        break;
-                    }
-                }
-                else{ // 한 칸 이상 차이
-                    flag = false;
+        int cnt=1;
+        bool ismake = true;
+        for(int c=0; c+1<N; c++){
+            if(map[r][c] == map[r][c+1]) cnt++;
+            else if(map[r][c]-map[r][c+1] == 1){ // 높이가 낮아지는 경우
+                if(cnt >= 0) cnt = 1-L;
+                else{
+                    ismake = false;
                     break;
                 }
             }
-            else{ // 이전 칸과 높이가 같은 경우
-                l++;
-                if(upToDown && l>=L){ // 위->아래로 경사로를 놓아야하고, 놓을 수 있는 경우
-                    l = 0;
-                    upToDown = false;
-                }
+            else if(map[r][c]-map[r][c+1] == -1){ // 높이가 높아지는 경우
+                if(cnt >= L) cnt = 1;
+                else {
+                    ismake = false;
+                    break;
+                }  
+            }
+            else { // 높이 차가 1 이상인 경우
+                ismake = false;
+                break;
             }
         }
-        if(l<L && upToDown) flag = false; // 위-> 아래로 경사로를 완성하지 못하는 경우
-        if(flag) ans++; // 지나갈 수 있는 경우
+        if(cnt>=0 && ismake) ans++;
     }
 
-    // col
-    for(int r=0; r<N ; r++){ 
+    // column check
+    for(int c=0; c<N ; c++){ 
 
-        int l=0;
-        bool flag = true;
-        bool leftToRight = false;
-        for(int c=0; c<N; c++){
-            if(c!=0 && map[r][c-1] != map[r][c]){ // 이전 칸과 높이가 같지 않은 경우
-                if(map[r][c-1]-map[r][c] == 1){ // \ (왼->오)
-                    if(leftToRight){ // 아직 이전 경사로를 놓지 못한 경우
-                        flag = false;
-                        break;
-                    }
-                    if(L!=1){
-                        leftToRight = true; // 왼->오 경사로 놓아야 함
-                        l = 1;
-                    }
-                    else l = 0;
-                }
-                else if(map[r][c-1]-map[r][c] == -1){ // / (오->왼)
-                    if(l>=L && !leftToRight) l = 1; // 오->왼 경사로를 놓을 수 있는 경우
-                    else{ // 경사로를 놓을 수 없는 경우
-                        flag = false;
-                        break;
-                    }
-                }
-                else{ // 한 칸 이상 차이
-                    flag = false;
+        int cnt=1;
+        bool ismake = true;
+        for(int r=0; r+1<N; r++){
+            if(map[r][c] == map[r+1][c]) cnt++;
+            else if(map[r][c]-map[r+1][c] == 1){ // 높이가 낮아지는 경우
+                if(cnt >= 0) cnt = 1-L;
+                else{
+                    ismake = false;
                     break;
                 }
             }
-            else{ // 이전 칸과 높이가 같은 경우
-                l++;
-                if(leftToRight && l>=L){ // 오->왼 경사로를 놓아야하고, 놓을 수 있는 경우
-                    l = 0;
-                    leftToRight = false;
-                }
+            else if(map[r][c]-map[r+1][c] == -1){ // 높이가 높아지는 경우
+                if(cnt >= L) cnt = 1;
+                else {
+                    ismake = false;
+                    break;
+                } 
+            }
+            else { // 높이 차가 1 이상인 경우
+                ismake = false;
+                break;
             }
         }
-        if(l<L && leftToRight) flag = false; // 왼->오 경사로를 완성하지 못하는 경우
-        if(flag) ans++; // 지나갈 수 있는 경우
+        if(cnt>=0 && ismake) ans++;
     }
 
     cout << ans;
