d0c351a044c3a60ef98ff04849a16c19872320fd
LIMDANBI <dlaeksql08222@gmail.com>
2023-02-21 00:50:44 +0900

    회전 초밥(브루트포스 알고리즘/두 포인터/슬라이딩 윈도우)

---
+++ BOJ/2531.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#define DISH 30001
+#define KIND 3001
+using namespace std;
+
+int N, d, k, c; // 회전 초밥 벨트에 놓인 접시의 수, 초밥의 가짓수, 연속해서 먹는 접시의 수, 쿠폰 번호
+int sushi[DISH], cnt[KIND];
+
+void input(){
+    cin >> N >> d >> k >> c;
+    for(int i=0; i<N; i++) cin >> sushi[i];
+}
+
+void solution(){ // 주어진 회전 초밥 벨트에서 먹을 수 있는 초밥의 가짓수의 최댓값
+    
+    int ans = 0, kind = 0;
+    
+    for(int i=0; i<k; i++){ // 처음 k개 
+        if(cnt[sushi[i]] == 0) kind++;
+        cnt[sushi[i]]++;
+    }
+
+    int left = 0, right = k-1; // two pointer
+    while(1){
+
+        // left
+        cnt[sushi[left]]--;
+        if(cnt[sushi[left]] == 0) kind--; // 종류 한 개 감소
+        left++;
+
+        //right
+        right++; right%=N;
+        if(right == k-1) break; // 회전 초밥 벨트 모두 확인 
+        cnt[sushi[right]]++;
+        if(cnt[sushi[right]] == 1) kind++; // 종류 한개 증가
+
+        // ans update 
+        if(ans <= kind){
+            ans = kind;
+            if(cnt[c]==0) ans++;
+        }
+    }
+    cout << ans;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    freopen("input.txt", "r", stdin);
+    input();
+    solution();
+}
