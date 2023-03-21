4ebc2e1150d3b7dde3e4201c682b7ad7a9326a78
LIMDANBI <dlaeksql08222@gmail.com>
2023-03-21 21:13:44 +0900

    1로 만들기 2(다이나믹 프로그래밍/그래프 이론/그래프 탐색)

--- BOJ/12852.cpp
+++ BOJ/12852.cpp
@@ -10,35 +10,30 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cin >> N;
-    if (N == 1)
-    {
-        cout << 0 << "\n"
-             << 1;
-        return 0;
-    }
 
-    dp[1] = dp[2] = dp[3] = 1;
-    for (int i = 4; i <= N; i++)
+    dp[1] = 0;
+    for (int i = 2; i <= N; i++)
     {
         dp[i] = dp[i - 1] + 1;
-        if (i % 3 == 0)
-            dp[i] = min(dp[i], dp[i / 3] + 1);
-        if (i % 2 == 0)
-            dp[i] = min(dp[i], dp[i / 2] + 1);
+        if (i % 2 == 0 && dp[i / 2] + 1 < dp[i])
+            dp[i] = dp[i / 2] + 1;
+
+        if (i % 3 == 0 && dp[i / 3] + 1 < dp[i])
+            dp[i] = dp[i / 3] + 1;
     }
     cout << dp[N] << "\n";
-    cout << N << " ";
 
-    int before = N;
     int nextCnt = dp[N] - 1;
-    for (int now = N - 1; now > 1 && nextCnt; now--) // 역추적 해가며 N을 1로 만드는 방법에 포함되어 있는 수를 출력
+    while (N != 0)
     {
-        if ((before - now == 1 || (before / 2 == now && before % 2 == 0) || (before / 3 == now && before % 3 == 0)) && dp[now] == nextCnt)
-        {
-            nextCnt--;
-            before = now;
-            cout << now << " ";
-        }
+        cout << N << " ";
+        if (N % 3 == 0 && dp[N / 3] == nextCnt)
+            N /= 3;
+        else if (N % 2 == 0 && dp[N / 2] == nextCnt)
+            N /= 2;
+
+        else
+            N--;
+        nextCnt--;
     }
-    cout << 1;
 }
