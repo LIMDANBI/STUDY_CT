60a2a49945a96fef22c4c523b2f4fe74e614afe7
LIMDANBI <dlaeksql08222@gmail.com>
2023-03-23 21:07:26 +0900

    진법 변환 2(수학/구현)

--- BOJ/11005.cpp
+++ BOJ/11005.cpp
@@ -4,18 +4,22 @@ using namespace std;
 
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     int N, B;
     cin >> N >> B;
 
-    vector<int> v;
-    while (N / B)
+    if (N == 0)
+        cout << 0;
+    else
     {
-        v.push_back(N % B);
-        N /= B;
+        vector<int> v;
+        while (N)
+        {
+            v.push_back(N % B);
+            N /= B;
+        }
+        for (int i = v.size() - 1; i >= 0; i--)
+            v[i] < 10 ? cout << v[i] : cout << (char)(v[i] + 'A' - 10);
     }
-    if (N != 0)
-        v.push_back(N);
-
-    for (int i = v.size() - 1; i >= 0; i--)
-        v[i] < 10 ? cout << v[i] : cout << (char)(v[i] + 'A' - 10);
 }
