20b1430d5ed03dd168f1b406c8783142d69d1e92
LIMDANBI <dlaeksql08222@gmail.com>
2023-12-19 10:28:01 +0900

    최소 비용으로 포장 다시 하기(정렬/우선순위 큐)

---
+++ 기타/최소 비용으로 포장 다시 하기.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int N, ni;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+void input(){
+    cin >> N;
+    for(int i=1; i<=N; i++){
+        cin >> ni;
+        pq.push(ni);
+    }
+}
+
+void solve(){
+    int ans = 0;
+    while (!pq.empty()){
+        int a = pq.top(); pq.pop();
+        if(pq.empty()) break;
+        else{
+            int b = pq.top(); pq.pop();
+            pq.push(a+b);
+            ans+=(a+b);
+        }
+    }
+    cout << ans;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    input();
+    solve();
+}
