8a9e94d10d378cbd529704a9c5aa326b7245e7de
LIMDANBI <dlaeksql08222@gmail.com>
2024-01-03 08:38:22 +0900

    건물세우기(BASIC2) (dfs/백트래킹)

---
+++ 기타/건물세우기(BASIC2).cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#define MAX 11
+using namespace std;
+
+int N, ans=10001;
+bool visited[MAX];
+int cost[MAX][MAX];
+
+void input(){
+    cin >> N;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++) cin >> cost[i][j];
+    }
+}
+
+void solve(int cnt, int sum){ // 각 빌딩을 어느 장소에 세우면 비용의 합이 최소가 되는지
+    if(ans < sum) return;
+    if(cnt == N){
+        ans = min(ans, sum);
+        return;
+    }
+    for(int idx=1; idx<=N; idx++){
+        if(visited[idx]) continue;
+        visited[idx] = true;
+        solve(cnt+1, sum+cost[cnt][idx]);
+        visited[idx] = false;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    input();
+    solve(0, 0);
+    cout << ans;
+}
