708a2717b076dc6b6d33deb727a977b068f39159
LIMDANBI <dlaeksql08222@gmail.com>
2024-01-02 11:02:55 +0900

    더하기(dfs/백트래킹)

--- 기타/더하기.cpp
+++ 기타/더하기.cpp
@@ -4,18 +4,23 @@ using namespace std;
 
 int N, K;
 int num[MAX];
+int psum[MAX]; // 구간합
 bool used[MAX];
 bool isPossible;
 
 void input(){
     cin >> N >> K;
     isPossible = false; // init
-    for(int i=0; i<N; i++) cin >> num[i];
+    for(int i=0; i<N; i++) {
+        cin >> num[i];
+        if(i==0) psum[i] = num[i]; 
+        else psum[i] = psum[i-1] + num[i];
+    }
 }
 
 void check(int cnt, int sum){
     if(sum == K) isPossible = true;
-    if(isPossible || cnt == N) return;
+    if(isPossible || cnt == N || sum+psum[N-1]-psum[cnt-1] < K) return; // 이미 만들어진 경우, 모든 수를 본 경우, 남은 수를 다 더해도 만들 수 없는 경우!!
 
     check(cnt+1, sum); // 해당 수를 더하지 않아봄
     check(cnt+1, sum+num[cnt]); // 해당 수를 더해봄
