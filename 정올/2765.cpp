f298b4b1d2c51b778e17e9669987faf0a8bd9dfa
LIMDANBI <dlaeksql08222@gmail.com>
2024-01-04 11:08:52 +0900

    미술관람 대회(dfs)

--- 정올/2765.cpp
+++ 정올/2765.cpp
@@ -16,26 +16,14 @@ void input(){
     }
 }
 
-void rgb(int y, int x, char c){ // 일반 꿀꿀이
+void dfs(int y, int x, char c){
     visited[y][x] = true;
     for(int d=0; d<4; d++){
         int ny = y+dy[d];
         int nx = x+dx[d];
         if(ny<0 || ny>=N || nx<0 || nx>=N) continue;
         if(visited[ny][nx] || map[ny][nx]!=c) continue;
-        rgb(ny, nx, c);
-    }
-}
-
-void rb(int y, int x, char c){ // 적록색맹 꿀꿀이
-    visited[y][x] = true;
-    for(int d=0; d<4; d++){
-        int ny = y+dy[d];
-        int nx = x+dx[d];
-        if(ny<0 || ny>=N || nx<0 || nx>=N || visited[ny][nx]) continue;
-        if((map[ny][nx] =='R' || map[ny][nx] =='G') && c == 'B') continue;
-        if((c=='R' || c=='G') && map[ny][nx] == 'B') continue;
-        rb(ny, nx, c);
+        dfs(ny, nx, c);
     }
 }
 
@@ -46,20 +34,25 @@ void solve(){
     for(int i=0; i<N; i++){
         for(int j=0; j<N; j++){
             if(visited[i][j]) continue;
-            rgb(i, j, map[i][j]);
+            dfs(i, j, map[i][j]);
             rgbCnt++;
         }
     }
 
     // init
     memset(visited, false, sizeof(visited));
+    for(int i=0; i<N; i++){ // red -> green
+        for(int j=0; j<N; j++){
+            if(map[i][j] == 'R') map[i][j] = 'G';
+        }
+    }
 
     // 적록색맹 꿀꿀이
     int rbCnt = 0;
     for(int i=0; i<N; i++){
         for(int j=0; j<N; j++){
             if(visited[i][j]) continue;
-            rb(i, j, map[i][j]);
+            dfs(i, j, map[i][j]);
             rbCnt++;
         }
     }
