b1adad20a3a11765e1f553480ec40fc6569e1683
LIMDANBI <dlaeksql08222@gmail.com>
2024-01-05 09:31:22 +0900

    둘레(Bronze)(dfs)

--- 기타/둘레(Bronze).cpp
+++ 기타/둘레(Bronze).cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int N, r, c;
 bool hay[MAX][MAX];
-bool outdoor[MAX][MAX];
+bool visited[MAX][MAX];
 int dy[] = {-1, 1, 0, 0};
 int dx[] = {0, 0, -1, 1};
 
@@ -16,31 +16,22 @@ void input(){
     }
 }
 
-void check_outdoor(){
-    for(int i=0; i<MAX; i++){
-        for(int j=0; j<MAX && !hay[i][j]; j++) outdoor[i][j] = true;
-        for(int j=MAX-1; j>=0 && !hay[i][j]; j--) outdoor[i][j] = true;
+int dfs(int y, int x){ // '건초 더미 밖 빈칸'을 기준으로 확산
+    int res = 0;
+    visited[y][x] = true;
+    for(int d=0; d<4; d++){
+        int ny = y+dy[d];
+        int nx = x+dx[d];
+        if(ny<0 || ny>=MAX || nx<0 || nx>=MAX) continue;
+        if(hay[ny][nx]) res++;
+        if(hay[ny][nx] || visited[ny][nx]) continue;
+        res += dfs(ny, nx);
     }
-}
-
-void get_boundary(){
-    int ans = 0;
-    for(int y=1; y<MAX-1; y++){
-        for(int x=1; x<MAX-1; x++){
-            if(!hay[y][x]) continue;
-            for(int d=0; d<4; d++){
-                int ny = y+dy[d];
-                int nx = x+dx[d];
-                if(outdoor[ny][nx]) ans++;
-            }
-        }
-    }
-    cout << ans;
+    return res;
 }
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
     input();
-    check_outdoor();
-    get_boundary();
+    cout << dfs(0, 0);
 }
