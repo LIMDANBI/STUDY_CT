54db05961b11f60f1e17a5de4178813ba2a7765e
LIMDANBI <dlaeksql08222@gmail.com>
2024-01-05 10:14:10 +0900

    문자열 폭발(자료 구조/문자열/스택)

--- BOJ/9935.cpp
+++ BOJ/9935.cpp
@@ -2,7 +2,7 @@
 #include<deque>
 using namespace std;
 
-deque<char> s1, s2;
+deque<char> s, tmp;
 string str, bombstr;
 
 void input(){
@@ -11,33 +11,33 @@ void input(){
 
 void solve(){
     for (int i = 0; i < str.size(); i++){
-        s1.push_back(str[i]);
-        if(bombstr.size() <= s1.size()){
+        s.push_back(str[i]);
+        if(bombstr.size() <= s.size()){ // 폭발 문자열의 길이 요건 충족
             bool ismatch = true;
-            for (int j = bombstr.size() - 1; j >= 0; j--){
-                if(s1.back() == bombstr[j]){
-                    s2.push_back(s1.back());
-                    s1.pop_back();
+            for (int j = bombstr.size() - 1; j >= 0; j--){ // 가장 최신 bombstr.size()개를 비교하여 폭발 문자열인지 확인
+                if(s.back() == bombstr[j]){
+                    tmp.push_back(s.back()); // 폭발 문자열이 아닌 경우, 다시 메인 스택에 저장해주기위해 임시 저장
+                    s.pop_back();
                 }
-                else{
+                else{ // 폭발 문자열과 match되지 않는 경우
                     ismatch = false;
-                    while (!s2.empty()){
-                        s1.push_back(s2.back());
-                        s2.pop_back();
+                    while (!tmp.empty()){ // 임시 스택에 저장해둔 폭발 문자열이 아닌 문자열을 다시 메인 스택에 옮겨줌
+                        s.push_back(tmp.back());
+                        tmp.pop_back();
                     }
-                    break;
+                    break; // 더 이상 볼 필요 없음
                 }
             }
-            while (ismatch && !s2.empty()) s2.pop_back();
+            while (ismatch && !tmp.empty()) tmp.pop_back();  // 폭발 문자열을 저장해둔 임시 stack을 비워줌
         }
     }
 }
 
 void output(){
     string res = "";
-    while (!s1.empty()){
-        res += s1.front();
-        s1.pop_front();
+    while (!s.empty()){
+        res += s.front();
+        s.pop_front();
     }
     res == "" ? cout << "FRULA" : cout << res;
 }
