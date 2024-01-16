553657b3753073c4522bcfa98d8b75fc8d791f87
LIMDANBI <dlaeksql08222@gmail.com>
2024-01-16 14:36:10 +0900

    버스 갈아타기(그래프 이론/그래프 탐색/기하학/너비 우선 탐색/선분 교차 판정)

--- 기타/버스 갈아타기.cpp
+++ 기타/버스 갈아타기.cpp
@@ -4,68 +4,62 @@
 #define MAX 5001
 using namespace std;
 
-struct QUEUE{int x, y, cnt;};
-struct BUS{int x1, y1, x2, y2;} bus[MAX];
+struct BUS{
+    int x1, y1, x2, y2;
+    bool is_in(const int &x, const int &y){
+        return (x1<=x && x<=x2 && y1<=y && y <=y2);
+    }
+    bool is_in(const BUS &b){
+        return (b.x1<=x2 && x1<=b.x2 && b.y1<=y2 && y1<=b.y2);
+    }
+};
 
 int M, N, K;
-bool used[MAX];
+BUS bus[MAX];
+int usedcnt[MAX];
 int sx, sy, dx, dy; // 출발지, 목적지
 
 void input(){
     cin >> M >> N >> K;
-    int id, x1, y1, x2, y2;
+    int b, x1, y1, x2, y2;
     for(int i=0; i<K; i++){
-        cin >> id >> x1 >> y1 >> x2 >> y2;
+        cin >> b >> x1 >> y1 >> x2 >> y2;
         if(x1>x2) swap(x1, x2);
         if(y1>y2) swap(y1, y2);
-        bus[id] = {x1, y1, x2, y2};
+        bus[b] = {x1, y1, x2, y2};
     }
     cin >> sx >> sy >> dx >> dy;
 }
 
-int solve(){ // 이용하는 최소 버스 수
-
-    queue<QUEUE> q;
+int bfs(){ // 이용하는 최소 버스 수
+    
+    queue<int> q;
 
     // 1. 처음 탈 수 있는 버스 queue에 넣기
-    for(int id=1; id<=K; id++){
-        if(bus[id].x1<=sx && sx<=bus[id].x2 && bus[id].y1<=sy && sy<=bus[id].y2){
-            used[id] = true;
-            for(int s=bus[id].x1; s<=bus[id].x2; s++){
-                for(int e=bus[id].y1; e<=bus[id].y2; e++) {
-                    if(s==sx && e==sy) continue;
-                    q.push({s, e, 1});
-                }
-            }
-        }
+    for(int b=1; b<=K; b++){
+        if(!bus[b].is_in(sx, sy)) continue;
+        usedcnt[b] = 1;
+        q.push(b);
     }
 
     // 2. bfs
     while(!q.empty()){
-        int x = q.front().x;
-        int y = q.front().y;
-        int cnt = q.front().cnt;
-        q.pop();
-
-        if(x==dx && y==dy) return cnt;
-        for(int id=1; id<=K; id++){
-            if(!(bus[id].x1<=x && x<=bus[id].x2 && bus[id].y1<=y && y<=bus[id].y2) || used[id]) continue;
-            used[id] = true;
-            for(int s=bus[id].x1; s<=bus[id].x2; s++){
-                for(int e=bus[id].y1; e<=bus[id].y2; e++) {
-                    if(s==sx && e==sy) continue;
-                    q.push({s, e, cnt+1});
-                }
+        int now = q.front(); q.pop();
+        if(bus[now].is_in(dx, dy)) return usedcnt[now];
+        
+        for(int b=1; b<=K; b++){
+            if(usedcnt[b]) continue;
+            if(bus[now].is_in(bus[b])){
+                usedcnt[b] = usedcnt[now]+1;
+                q.push(b);
             }
         }
     }
-    
-    return -1;
 }
 
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
     input();
-    cout << solve();
+    cout << bfs();
 }
