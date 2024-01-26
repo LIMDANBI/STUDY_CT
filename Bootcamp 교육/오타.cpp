a95afb311f883043f4ca81209052cb38b7ac2c75
LIMDANBI <dlaeksql08222@gmail.com>
2024-01-26 13:48:34 +0900

    오타(구현)

--- Bootcamp 교육/오타.cpp
+++ Bootcamp 교육/오타.cpp
@@ -8,44 +8,23 @@ string str;
 void input(){
     cin >> str;
 }
-
-int isBalance(){
-    int st = 0; // '(' 개수 저장
-    for(char c : str){
-        if(c == '(') st++;
-        else st--;
-    }
-    return st;
-}
-
-int getCnt(int dir){ // 전체 문자열이 균형 잡히게 되는 위치의 수 구하기
-    int ans = 0, st = 0;
-    if(dir < 0){
-        for(char c : str){
-            if(c == '(') st++;
-            else{
-                ans++;
-                if(--st<0) break;
-            }
-        }
-    }
-    else{
-        reverse(str.begin(), str.end()); // 뒤에서부터 보기위해
-        for(char c : str){
-            if(c == ')') st++;
-            else{
-                ans++;
-                if(--st<0) break;
-            }
-        }
-    }
-    return ans;
-}
  
 int solve(){
-    int res = isBalance();
-    if(res == 0) cout << 0; 
-    else cout << getCnt(res);
+    int lev=0, open=0, close=0;
+    for(char c : str){
+        if(c == '('){
+            lev++;
+            open++;
+        }
+        else{
+            lev--;
+            close++;
+        }
+
+        if(lev == 1) open =0;
+        if(lev  < 0) return close;
+    }
+    return open;
 }
  
 int main(){
