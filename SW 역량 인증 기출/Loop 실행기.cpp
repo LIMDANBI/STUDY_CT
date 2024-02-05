42f577efc0aef85b43de35c1147a1a474d4677cf
LIMDANBI <dlaeksql08222@gmail.com>
2024-02-05 16:52:44 +0900

    Loop 실행기(재귀)

--- SW 역량 인증 기출/Loop 실행기.cpp
+++ SW 역량 인증 기출/Loop 실행기.cpp
@@ -2,44 +2,31 @@
 #include<string>
 using namespace std;
 
-void print_loop(string loop){ // Loop문 작업 실행 순서 출력
+string loop;
 
-    int cnt = loop[1] - '0'; // 반복 횟수
-    int size = (int)loop.size();
-    
-    for (int c = 0; c < cnt; c++){
-        int idx = 2;
-        string str = "";
-        while(idx < size){
-            if(loop[idx] == '<'){ // 중첩 loop가 시작되는 경우
-                // 이전까지 출력 처리
-                cout << str;
-                str = "";
-
-                // 중첩 loop
-                int len = 0;
-                while(loop[idx + len] != '>') len++;
-                print_loop(loop.substr(idx, len));
-                idx += len;
-            }
-            else if(loop[idx]!='>') str += loop[idx];
-            idx++;
-        }
-        cout << str;
-    }
-}
-
-void solve() {
-
-    // 1. 입력
-    string loop;
+void input(){
     cin >> loop;
-
-    // 2. Loop문 작업 실행 순서 출력
-    print_loop(loop);
 }
+
+int print_loop(int idx){ // Loop문 작업 실행 순서 출력
+    int start;
+    int cnt = loop[idx+1]-'0';
+    while (cnt--){ // 반복 횟수
+        start = idx+2; 
+        while (loop[start] != '>'){
+            if(loop[start] == '<'){ // 중첩 loop인 경우
+                start = print_loop(start);
+            }
+            else cout << loop[start];
+            start++;
+        }
+    }
+    return start;
+}
+
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
-    solve();
+    input();
+    print_loop(0);
 }
