62c558a70ac031021fdf85a99577feabe084edcd
LIMDANBI <dlaeksql08222@gmail.com>
2024-02-21 22:48:10 +0900

    숨바꼭질 2(그래프 이론/그래프 탐색/너비 우선 탐색)

--- BOJ/12851.cpp
+++ BOJ/12851.cpp
@@ -12,13 +12,8 @@ void input(){
 
 void solve(){
 
-    if(N==K){ // 둘의 시작 위치가 같은 경우
-        cout << 0 << "\n" << 1;
-        return;
-    }
-
     // 1. 초기화
-    int cnt = 0; // 방법 개수
+    int cnt = 1; // 방법 개수
     for (int i = 0; i < MAX; i++) cost[i] = MAX; // 가장 빠르게 찾을 수 있는 시간
 
     // 2. 시작점 처리
@@ -33,23 +28,23 @@ void solve(){
 
         // 앞으로 걷기
         if(cur+1<MAX && cost[cur]+1<=cost[cur+1]){
-            cost[cur + 1] = cost[cur] + 1;
-            if(cur+1==K) cost[cur]+1 == cost[cur+1] ? cnt++ : cnt=1;
+            if(cur+1==K) (cost[cur]+1 == cost[cur+1]) ? cnt++ : cnt=1;
             else q.push(cur + 1);
+            cost[cur + 1] = cost[cur] + 1;
         }
 
         // 뒤로 걷기
         if(0<=cur-1 && cost[cur]+1<=cost[cur-1]){
-            cost[cur - 1] = cost[cur] + 1;
-            if(cur-1==K) cost[cur]+1 == cost[cur-1] ? cnt++ : cnt=1;
+            if(cur-1==K) (cost[cur]+1 == cost[cur-1]) ? cnt++ : cnt=1;
             else q.push(cur - 1);
+            cost[cur - 1] = cost[cur] + 1;
         }
 
         // 순간이동
         if(cur*2<MAX && cost[cur]+1<=cost[cur*2]){
-            cost[cur * 2] = cost[cur] + 1;
-            if(cur*2==K) cost[cur]+1 == cost[cur*2] ? cnt++ : cnt=1;
+            if(cur*2==K) (cost[cur]+1 == cost[cur*2]) ? cnt++ : cnt=1;
             else q.push(cur * 2);
+            cost[cur * 2] = cost[cur] + 1;
         }
     }
 
@@ -59,7 +54,7 @@ void solve(){
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
-    freopen("../test/input.txt", "r", stdin);
+    // freopen("../test/input.txt", "r", stdin);
     input();
     solve();
 }
